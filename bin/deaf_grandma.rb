166b8502e0a53a6069591d92d95bcce6f3b837aa
BlueVajra <cory.leistikow@gmail.com>
2014-03-08 11:52:27 -0700

    Grandma randomizes years. All systems go

--- bin/deaf_grandma.rb
+++ bin/deaf_grandma.rb
@@ -2,7 +2,7 @@ puts "Say something to Grandma"
 statement = gets.chomp
 statementup = statement.upcase
 if statement == statementup
-  puts "NO, NOT SINCE 1932!"
+  puts "NO, NOT SINCE #{rand(1930..1950)}!"
 else
   puts "HUH?! SPEAK UP SONNY!"
 end
