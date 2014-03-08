7dc06522a06bfd935e70766b6f52a6f512c56da9
BlueVajra <cory.leistikow@gmail.com>
2014-03-08 11:44:08 -0700

    Grandma hears shouting

--- bin/deaf_grandma.rb
+++ bin/deaf_grandma.rb
@@ -1,4 +1,8 @@
 puts "Say something to Grandma"
 statement = gets.chomp
-
-puts "HUH?! SPEAK UP SONNY!"
+statementup = statement.upcase
+if statement == statementup
+  puts "NO, NOT SINCE 1932!"
+else
+  puts "HUH?! SPEAK UP SONNY!"
+end
