db286cc8f23717e47c7233cf50f63273f60a22df
BlueVajra <cory.leistikow@gmail.com>
2014-03-08 14:04:18 -0700

    roman numeral code cleanup

--- bin/roman_numerals.rb
+++ bin/roman_numerals.rb
@@ -4,11 +4,11 @@ this_number = gets.chomp.to_i
 roman_numerals = {1 => "I", 4 => "IV", 5 => "V", 9 => "IX", 10 => "X", 40 => "XL", 50 => "L", 90 => "XC", 100 => "C", 400 => "CD", 500 => "D", 900 => "CM", 1000 => "M"}
 output = ""
 current_key = 1
-current_number=this_number
+current_number = this_number
 
-while current_number>0
+while current_number > 0
   roman_numerals.each do |key, value|
-    if  current_number>=key
+    if current_number >= key
       current_key = key
     end
   end
