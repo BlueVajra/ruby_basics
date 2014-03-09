67b4ce0388baadd56e6ab17e23ef48973092c2f3
BlueVajra <cory.leistikow@gmail.com>
2014-03-09 12:23:23 -0600

    cleared up variable names

--- bin/deaf_grandma.rb
+++ bin/deaf_grandma.rb
@@ -1,7 +1,7 @@
 puts "Say something to Grandma"
 statement = gets.chomp
-statementup = statement.upcase
-if statement == statementup
+statement_up = statement.upcase
+if statement == statement_up
   puts "NO, NOT SINCE #{rand(1930..1950)}!"
 else
   puts "HUH?! SPEAK UP SONNY!"
