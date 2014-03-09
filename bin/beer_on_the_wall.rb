1a5909d9261fb6c1be179bd8f4eb5734aa2910fe
BlueVajra <cory.leistikow@gmail.com>
2014-03-09 12:07:39 -0600

    Cleaned up formatting

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -7,12 +7,12 @@ else
     puts "No bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
   else
     while cur_num > 0
-      next_num = (cur_num-1 != 0) ? cur_num-1 : "no"
+      next_num = (cur_num - 1 != 0) ? cur_num - 1 : "no"
       plural_one = (cur_num == 1) ? "" : "s"
       plural_two = (next_num == 1) ? "" : "s"
 
       puts "#{cur_num} bottle#{plural_one} of beer on the wall, #{cur_num} bottle#{plural_one} of beer.\nTake one down and pass it around, #{next_num} bottle#{plural_two} of beer on the wall.\n"
-      cur_num = cur_num-1
+      cur_num = cur_num - 1
     end
   end
 end
